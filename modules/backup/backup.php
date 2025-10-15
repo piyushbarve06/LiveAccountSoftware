f054dc408130ea5da5ab5937e3c00401f0193459
piyushbarve06 <piyushbarve06@gmail.com>
2025-10-15 08:41:06 +0530

    chnages

--- modules/backup/backup.php
+++ modules/backup/backup.php
@@ -9,7 +9,9 @@ Version: 2.3.0
 Requires at least: 2.3.*
 */
 
-require(__DIR__ . '/vendor/autoload.php');
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require(__DIR__ . '/vendor/autoload.php');
+}
 
 define('BACKUP_MODULE_NAME', 'backup');
 
