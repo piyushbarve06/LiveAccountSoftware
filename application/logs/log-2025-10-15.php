08be4556774689d464458f9491cb0edcf97145a7
piyushbarve06 <piyushbarve06@gmail.com>
2025-10-15 08:19:48 +0530

    chnages

---
+++ application/logs/log-2025-10-15.php
@@ -0,0 +1,69 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2025-10-15 04:48:42 --> Config Class Initialized
+INFO - 2025-10-15 04:48:42 --> Hooks Class Initialized
+DEBUG - 2025-10-15 04:48:42 --> UTF-8 Support Enabled
+INFO - 2025-10-15 04:48:42 --> Utf8 Class Initialized
+INFO - 2025-10-15 04:48:42 --> URI Class Initialized
+INFO - 2025-10-15 04:48:42 --> Router Class Initialized
+INFO - 2025-10-15 04:48:42 --> Output Class Initialized
+INFO - 2025-10-15 04:48:43 --> Security Class Initialized
+DEBUG - 2025-10-15 04:48:43 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-10-15 04:48:43 --> CSRF cookie sent
+INFO - 2025-10-15 04:48:43 --> Input Class Initialized
+INFO - 2025-10-15 04:48:43 --> Language Class Initialized
+INFO - 2025-10-15 04:48:43 --> Config Class Initialized
+INFO - 2025-10-15 04:48:43 --> Loader Class Initialized
+INFO - 2025-10-15 04:48:43 --> Helper loaded: files_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: language_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: url_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: file_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: form_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: settings_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: modules_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: core_hooks_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: admin_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: assets_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: user_meta_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: emails_tracking_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: staff_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: countries_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: payment_gateways_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: general_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: misc_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: func_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: gdpr_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: datatables_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: table_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: custom_fields_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: menu_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: template_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: email_templates_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: invoices_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: subscriptions_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: estimates_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: contracts_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: credit_notes_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: proposals_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: projects_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: tasks_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: fields_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: leads_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: tickets_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: relation_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: tags_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: pdf_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: clients_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: database_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: upload_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: sales_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: themes_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: pre_query_data_formatters_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: widgets_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: sms_helper
+INFO - 2025-10-15 04:48:43 --> Helper loaded: deprecated_helper
+INFO - 2025-10-15 04:48:44 --> Language file loaded: language/english/english_lang.php
+INFO - 2025-10-15 04:48:44 --> Session: Class initialized using 'files' driver.
+INFO - 2025-10-15 04:48:44 --> Database Driver Class Initialized
+ERROR - 2025-10-15 04:48:45 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo for liveaccountsoftware_liveaccountsoftware failed: No such host is known.  C:\xampp\htdocs\LiveAccountSoftware\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2025-10-15 04:48:45 --> Severity: error --> Exception: php_network_getaddresses: getaddrinfo for liveaccountsoftware_liveaccountsoftware failed: No such host is known.  C:\xampp\htdocs\LiveAccountSoftware\system\database\drivers\mysqli\mysqli_driver.php 203
